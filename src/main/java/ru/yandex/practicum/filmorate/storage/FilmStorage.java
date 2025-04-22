e79da3c92c40c7f2be31fd85a9779074d474e032
Max <m.browckin@yandex.ru>
2025-04-22 10:31:33 +0300

    Переработана архитектура и добалена новая логика

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -4,12 +4,21 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     Film getFilmById(Long id);
+
     List<Film> getAllFilms();
+
     void removeFilm(Long id);
+
     void addLike(Long filmId, Long userId);
+
     void removeLike(Long filmId, Long userId);
+
     List<Film> getPopularFilms(int count);
+
 }
